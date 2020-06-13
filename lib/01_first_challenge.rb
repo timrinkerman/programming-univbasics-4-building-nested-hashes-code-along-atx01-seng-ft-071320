71f5e2ff08a501a279517c89904d4e6341b24b7b
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-13 02:27:47 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,3 +1,3 @@
 def first_challenge
-   epic_tragedy = { :montague => { :patriarch =>}}
+   epic_tragedy = { :montague => { :patriarch => { :name => "Lord Montague", :age => "53" }, :matriarch => { :name => "Lady Montague", :age => "54" }, :hero => { :name => "Romeo", :age => "15" }, :hero_friends => [ { :name => "Benvolio", :age => "17", :attitude => "worried" }, {}]}}
 end
