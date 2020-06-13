5c907e445fd9a6540b3277cf57a51fa0e68250b6
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-13 02:43:03 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,15 +1,14 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: => "Lord Montague", age: "53"},
+      :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        name: "Lord Capulet", age: "50"
-        },
+      :patriarch => {name: "Lord Capulet", age: "50"
+},
       :matriarch => {
         name: "Lady Capulet", age: "51"
         },
